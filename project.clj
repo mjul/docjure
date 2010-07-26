2a8182f4768f817c8ab45b52b89b6fc33e330345
Martin Jul <martin@mjul.com>
2010-07-26 10:42:17 +0200

    Changed version to 1.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject dk.ative/docjure "1.4.0"
+(defproject dk.ative/docjure "1.5.0-SNAPSHOT"
   :description "Easily read and write Office documents from Clojure."
   :url "http://github.com/ative/docjure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
 		 [org.apache.poi/poi "3.6"]
 		 [org.apache.poi/poi-ooxml "3.6"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]
-		     [lein-clojars "0.5.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-clojars "0.5.0"]
+		     [lein-difftest "1.2.2"]]
+  )
