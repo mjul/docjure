4f182789f6042e90b9b49e08e514ab687d449615
Martin Jul <martin@mjul.com>
2010-09-19 02:24:31 +0200

    Upgraded to Clojure 1.2

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject dk.ative/docjure "1.5.0-SNAPSHOT"
   :description "Easily read and write Office documents from Clojure."
   :url "http://github.com/ative/docjure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.poi/poi "3.6"]
 		 [org.apache.poi/poi-ooxml "3.6"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
 		     [lein-clojars "0.5.0"]
-		     [lein-difftest "1.2.2"]]
+		     [lein-difftest "1.3.0"]]
+  :hooks [leiningen.hooks.difftest]
   )
