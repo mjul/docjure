06ace2c6079d2fb846b3f995e933539e7c0e6d1a
Tom Pierce <tlpierce@gmail.com>
2011-11-14 14:52:28 -0600

    Updated to poi 3.8-beta4. All tests run successfully.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/ative/docjure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [org.apache.poi/poi "3.6"]
-		 [org.apache.poi/poi-ooxml "3.6"]]
+		 [org.apache.poi/poi "3.8-beta4"]
+		 [org.apache.poi/poi-ooxml "3.8-beta4"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
 		     [lein-clojars "0.6.0"]
 		     [lein-difftest "1.3.2-SNAPSHOT"]]
