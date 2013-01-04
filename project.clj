cf0c2d628567714abaf37b607fef48a358976800
Ragnar Dahlén <r.dahlen@gmail.com>
2013-01-04 11:51:12 +0000

    Add alias for 'all' for all profiles

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :plugins [[lein-difftest "2.0.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+  :aliases {"all" ["with-profile" "1.3:1.4:1.5"]})
