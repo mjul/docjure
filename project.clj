58644651fed215e26a1186005667bb418e27e713
Ragnar Dahlén <r.dahlen@gmail.com>
2013-01-04 11:42:16 +0000

    Update Apache POI version to 3.9

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Easily read and write Office documents from Clojure."
   :url "http://github.com/ative/docjure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [org.apache.poi/poi "3.6"]
-		 [org.apache.poi/poi-ooxml "3.6"]]
+		 [org.apache.poi/poi "3.9"]
+		 [org.apache.poi/poi-ooxml "3.9"]]
   :plugins [[lein-difftest "2.0.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
