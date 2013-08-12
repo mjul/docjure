d975f5eb27f56c4bb05b10ba9f29281474d17430
vijaykiran <mail@vijaykiran.com>
2013-08-12 11:04:13 +0200

    Bump clojure version number.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/ative/docjure"
   :license {:name "MIT License"
             :url "http://http://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 		 [org.apache.poi/poi "3.9"]
 		 [org.apache.poi/poi-ooxml "3.9"]]
   :plugins [[lein-difftest "2.0.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :aliases {"all" ["with-profile" "1.3:1.4:1.5"]})
