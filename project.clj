3529d86e90aee4379f77fb4c6bccf4774ec1c4f1
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-10-11 12:59:53 +0100

    Change to own clojars deployed artifact.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dk.ative/docjure "1.7.0-SNAPSHOT"
+(defproject tgk/docjure "1.7.0-SNAPSHOT"
   :description "Easily read and write Office documents from Clojure."
   :url "http://github.com/ative/docjure"
   :license {:name "MIT License"
