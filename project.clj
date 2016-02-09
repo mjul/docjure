4e6c01e3e82bde118cbcae92088690e2f4d0d81d
Martin Jul <martin@mjul.com>
2016-02-09 23:00:34 +0100

    Upgrade to Clojure 1.8 and support 1.7, too

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mjul/docjure"
   :license {:name "MIT License"
             :url "http://http://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.poi/poi "3.13"]
                  [org.apache.poi/poi-ooxml "3.13"]]
   :plugins [[lein-difftest "2.0.0"]]
@@ -11,8 +11,10 @@
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :test {:global-vars  {*warn-on-reflection* false}
                     :dependencies [[com.cemerick/pomegranate "0.3.0"]]}}
-  :aliases {"all" ["with-profile" "1.3:1.4:1.5:1.6"]}
+  :aliases {"all" ["with-profile" "1.3:1.4:1.5:1.6:1.7:1.8"]}
   :global-vars {*warn-on-reflection* true})
 
