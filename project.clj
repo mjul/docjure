b5d47ec4ff9e12cc47be76e33ba495c496acbddc
Alexander Scott <xander@cadcoder.com>
2018-10-02 20:53:32 +1000

    Add support for Clojure 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mjul/docjure"
   :license {:name "MIT License"
             :url "http://http://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.poi/poi "3.17"]
                  [org.apache.poi/poi-ooxml "3.17"]]
   :plugins [[lein-difftest "2.0.0"]]
@@ -11,8 +11,9 @@
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :test {:global-vars  {*warn-on-reflection* false}
                     :dependencies [[com.cemerick/pomegranate "0.3.0"]]}}
-  :aliases {"all" ["with-profile" "1.5:1.6:1.7:1.8"]}
+  :aliases {"all" ["with-profile" "1.5:1.6:1.7:1.8:1.9"]}
   :global-vars {*warn-on-reflection* true})
 
