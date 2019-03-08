809ec3e0f861d7801e55e478560dfecc519d3a5e
Manuel Herzog <herzog@xmediasports.com>
2019-03-08 16:18:47 +0100

    Added clojure 1.10

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject dk.ative/docjure "1.14.0-SNAPSHOT"
+(defproject dk.ative/docjure "1.14.1-SNAPSHOT"
   :description "Easily read and write Office documents from Clojure."
   :url "http://github.com/mjul/docjure"
   :license {:name "MIT License"
             :url "http://http://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.apache.poi/poi "4.0.1"]
                  [org.apache.poi/poi-ooxml "4.0.1"]]
   :plugins [[lein-difftest "2.0.0"]]
@@ -12,7 +12,8 @@
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
              :test {:global-vars  {*warn-on-reflection* false}}}
-  :aliases {"all" ["with-profile" "1.5:1.6:1.7:1.8:1.9"]}
+  :aliases {"all" ["with-profile" "1.5:1.6:1.7:1.8:1.9:1.10"]}
   :global-vars {*warn-on-reflection* true})
 
