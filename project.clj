0b17b4e9cbfb9c107330904b0d80f3093d65b02b
Martin Jul <martin@mjul.com>
2024-06-17 15:21:11 +0200

    Upgrade to Clojure 1.11.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mjul/docjure"
   :license {:name "MIT License"
             :url "http://http://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.apache.poi/poi "5.2.3"]
                  [org.apache.poi/poi-ooxml "5.2.3"]]
   :plugins [[lein-difftest "2.0.0"]
@@ -15,7 +15,7 @@
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.3"]]}
              :test {:global-vars  {*warn-on-reflection* false}
                     :resource-paths ["test/dk/ative/docjure/testdata"]}}
   :aliases {"all" ["with-profile" "1.5:1.6:1.7:1.8:1.9:1.10:1.11"]}
